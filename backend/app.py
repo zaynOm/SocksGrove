f7efd8aa50003d68c8feff817e6a5efb6ba2081e
zaynOm <ouaziz0626@gmail.com>
2024-01-31 14:58:53 +0100

    init ✨

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+URL = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = URL
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def json(self):
+        return {'id': self.id,'username': self.username, 'email': self.email}
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return 'Hello world!'
+
+# create a user
+@app.route('/users', methods=['POST'])
+def create_user():
+  try:
+    data = request.get_json()
+    new_user = User(username=data['username'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return make_response(jsonify({'message': 'user created'}), 201)
+  except e:
+    return make_response(jsonify({'message': 'error creating user'}), 500)
+
+# get all users
+@app.get('/users')
+def get_users():
+  try:
+    users = User.query.all()
+    return make_response(jsonify([user.json() for user in users]), 200)
+  except e:
+    return make_response(jsonify({'message': 'error getting users'}), 500)
