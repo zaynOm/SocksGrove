159f8aa1efe835a2cb2ae64c6088b17b61574e7a
zaynOm <ouaziz0626@gmail.com>
2024-02-01 02:13:29 +0100

    entry point

--- backend/app.py
+++ backend/app.py
@@ -1,47 +1,13 @@
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+# backend/app.py
+from app import app, db
+from app.routes.user_routes import user_bp
 
-app = Flask(__name__)
-
-URL = getenv('DATABASE_URL')
-app.config['SQLALCHEMY_DATABASE_URI'] = URL
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def json(self):
-        return {'id': self.id,'username': self.username, 'email': self.email}
 
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def home():
-    return 'Hello world!'
+    # adding user routes bluprint to the app
+    app.register_blueprint(user_bp)
 
-# create a user
-@app.route('/users', methods=['POST'])
-def create_user():
-  try:
-    data = request.get_json()
-    new_user = User(username=data['username'], email=data['email'])
-    db.session.add(new_user)
-    db.session.commit()
-    return make_response(jsonify({'message': 'user created'}), 201)
-  except e:
-    return make_response(jsonify({'message': 'error creating user'}), 500)
-
-# get all users
-@app.get('/users')
-def get_users():
-  try:
-    users = User.query.all()
-    return make_response(jsonify([user.json() for user in users]), 200)
-  except e:
-    return make_response(jsonify({'message': 'error getting users'}), 500)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
