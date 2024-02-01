7c898de2daccf60cc6edbe5b9abf10126246e4f4
zaynOm <ouaziz0626@gmail.com>
2024-02-01 02:14:04 +0100

    init the flask app & db

---
+++ backend/app/__init__.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
